a091a1bd991d0b438f2f2c11d974dc1897565e7c
Ankush Banerjee <banerjeeankush184@gmail.com>
2024-04-12 15:36:35 +0530

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Periskope",
+  title: "Kanboard",
   description: "Developed by Ankush Banerjee",
 };
 
