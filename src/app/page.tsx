0b5b31ec4d27e6ddd401f6ad2fccc8d4a280af69
Ankush Banerjee <banerjeeankush184@gmail.com>
2024-04-12 20:32:16 +0530

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,6 +59,7 @@ function Page() {
       <div>
         <div className="flex justify-center my-20">
           <input
+            required
             className="border border-gray-300 rounded-md px-4 py-2 mt-2 focus:outline-none focus:border-blue-500"
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
